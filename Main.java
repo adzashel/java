d0aeb510f3d762fd6376b286c3fd2d46604047cc
adzashel <seigneur.noir15@gmail.com>
2025-05-06 01:06:54 +0700

    "Time Conversion"

--- Main.java
+++ Main.java
@@ -1,21 +1,23 @@
-import  java.util.Scanner;
+import  java.io.*;
 
+public  class Main {
+    public static void main(String[] args) throws  IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader rl = new BufferedReader(input);
 
-public class Main  {
-    public static  void  main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah menit: ");
+        long minute = Integer.parseInt(rl.readLine());
 
-        System.out.print("Masukan berapa lama parkir = ");
-        int time = input.nextInt();
-        int fee ;
-        if(time < 2 ) {
-            fee = 3000;
-            System.out.print("Biaya parkir = " + fee);
-        }else if( time > 2 ) {
-            fee = 3000 + ( time - 2 ) * 2000;
-            System.out.print("Biaya parkir = " + fee);
-        }else {
-            System.out.print("Input salah");
-        }
+        long toHour = minute / 60;
+        long toDay = toHour / 24;
+        long year = 365;
+        long toYear = toDay / year;
+        long yearRemainder = toDay % year; // cari sisa hari setelah 1 thn
+        // output year day
+
+        System.out.println("\nKonversi waktu:");
+        System.out.println(minute + " menit =  ");
+        System.out.println(toYear + " tahun " +  " dan " +  yearRemainder + " Hari ");
+        // tahun dan hari
     }
 }
